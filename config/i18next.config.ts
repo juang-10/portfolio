ec198dc50a6fb18f335f0c1da865903cff7e7968
juan <judagosa8520@gmail.com>
2025-03-02 12:14:03 -0500

    feat: add i18n resource support for English and Spanish translations in i18next configuration

--- config/i18next.config.ts
+++ config/i18next.config.ts
@@ -1,20 +1,21 @@
+import { enTranslation } from '@/public/locales/en/enTranslation';
+import { esTranslation } from '@/public/locales/es/esTranslation';
 import i18n from 'i18next';
 import Backend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 
 i18n
-  .use(Backend)
   .use(initReactI18next)
   .init({
+    resources: {
+      en: { translate: enTranslation },
+      es: { translate: esTranslation },
+    },
     fallbackLng: 'en',
     debug: true,
-    lng: 'en',
     ns: ['translate'],
     interpolation: {
       escapeValue: false,
-    },
-    backend: {
-      loadPath: '/locales/{{lng}}/{{ns}}.json',
     },
     react: {
       useSuspense: true,
